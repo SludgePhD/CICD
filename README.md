8555f92cac5e2fd779804b20d58e197da1d7ffe9
Sludge <96552222+SludgePhD@users.noreply.github.com>
2022-10-16 18:18:19 +0200

    multi-package support (untested)

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 A simple CI/CD tool for Rust crates.
 
 Based on <https://github.com/rust-analyzer/smol_str/blob/master/.github/ci.rs>.
-
-Limitations:
-- only works in single-crate workspaces
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{
     env, fs,
+    path::PathBuf,
     process::{self, Command, ExitStatus, Stdio},
     time::Instant,
 };
@@ -45,19 +46,74 @@ fn try_main() -> Result<()> {
     let current_branch = shell_output("git branch --show-current")?;
     if &current_branch == "main" {
         let _s = Section::new("PUBLISH");
-        let manifest = fs::read_to_string(&cargo_toml)?;
-        let version = get_field(&manifest, "version")?;
-        let tag = format!("v{}", version);
         let tags = shell_output("git tag --list")?;
 
-        if !tags.contains(&tag) {
-            let token = env::var("CRATES_IO_TOKEN").expect("no `CRATES_IO_TOKEN` provided");
-            shell(&format!("git tag v{}", version))?;
-            shell(&format!("cargo publish --token {}", token))?;
-            shell("git push --tags")?;
+        let packages = find_packages()?;
+        assert!(!packages.is_empty());
+
+        // Did any previous release include multiple packages?
+        let was_multi_package = packages.iter().any(|pkg| tags.contains(&pkg.name));
+        let is_multi_package = packages.len() != 1;
+
+        let needs_publish = |pkgname: &str, version: &str| {
+            if was_multi_package {
+                tags.contains(&format!("{pkgname}-v{version}"))
+            } else {
+                tags.contains(&format!("v{version}"))
+            }
+        };
+
+        for Package { name, version } in packages {
+            let prefix = if is_multi_package {
+                format!("{}-", name)
+            } else {
+                String::new()
+            };
+
+            if needs_publish(&name, &version) {
+                let tag = format!("{prefix}v{version}");
+                let token = env::var("CRATES_IO_TOKEN").expect("no `CRATES_IO_TOKEN` provided");
+                shell(&format!("git tag {tag}"))?;
+                shell(&format!("cargo publish -p {name} --token {}", token))?;
+                shell("git push --tags")?;
+            }
         }
     }
     Ok(())
+}
+
+struct Package {
+    name: String,
+    version: String,
+}
+
+fn find_packages() -> Result<Vec<Package>> {
+    fn recurse(dir: PathBuf, out: &mut Vec<Package>) -> Result<()> {
+        let mut toml = dir.clone();
+        toml.push("Cargo.toml");
+        if toml.exists() {
+            let manifest = fs::read_to_string(&toml)?;
+            // Filter out virtual manifests and those with `publish = ...` set.
+            if manifest.contains("[package]") && get_field(&manifest, "publish").is_err() {
+                let name = get_field(&manifest, "name")?.to_string();
+                let version = get_field(&manifest, "version")?.to_string();
+
+                out.push(Package { name, version });
+            }
+        }
+
+        for entry in fs::read_dir(&dir)? {
+            let entry = entry?;
+            if entry.file_type()?.is_dir() {
+                recurse(entry.path(), out)?;
+            }
+        }
+        Ok(())
+    }
+
+    let mut out = Vec::new();
+    recurse(env::current_dir()?, &mut out)?;
+    Ok(out)
 }
 
 fn get_field<'a>(text: &'a str, name: &str) -> Result<&'a str> {
